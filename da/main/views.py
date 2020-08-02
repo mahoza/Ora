581812cc49a87eac34e8a1311b2f4fa69bca9aa5
Mary <marygozha0704@gmail.com>
2020-08-02 13:29:53 +0300

    Add new files

--- da/main/views.py
+++ da/main/views.py
@@ -3,10 +3,15 @@ from .models import Text
 
 
 def index(request):
-    texts = Text.objects.all()
+    texts = Text.objects.order_by('-id')[:7]
     return render(request, 'main/index.html', {'title': 'General page', 'texts': texts})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def publish(request):
+    return render(request, 'main/publish.html')
+
 
