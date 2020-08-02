36fc3b656523b7022190f89f5c75f7ce651f9d7c
Mary <marygozha0704@gmail.com>
2020-08-02 18:23:03 +0300

    Add new files

--- da/ora/asgi.py
+++ da/ora/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for da project.
+ASGI config for ora project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'da.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ora.settings')
 
 application = get_asgi_application()
