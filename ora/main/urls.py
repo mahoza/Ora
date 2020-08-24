4eede7e8e0ab56f2cba9fb7b7de20b6bf85e2dfe
Mary <marygozha0704@gmail.com>
2020-08-24 17:12:08 +0300

    add login,signup,logout 1.0

--- ora/main/urls.py
+++ ora/main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('publish', views.publish, name='publish'),
     path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
 ]
--- ora/main/views.py
+++ ora/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 from .models import Text
 from .forms import TextForm
 
@@ -29,6 +31,35 @@ def publish(request):
     return render(request, 'main/publish.html', context)
 
 
-def login(request):
-    return render(request, 'main/login.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            #log the user in
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form= UserCreationForm()
+    return render(request, 'main/signup.html', {'form':form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log in the user
+            user=form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form= AuthenticationForm()
+    return render(request, 'main/login.html', {'form':form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
+
 
